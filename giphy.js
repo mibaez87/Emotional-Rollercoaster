19d7dcfdf608f06073fa304b6be626d36fdf96cb
Michel B <michelb@JB-MBA.local>
2017-12-04 16:13:00 -0500

    Working ajax

--- giphy.js
+++ giphy.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     var emotionsArray = ["UNIMPRESSED", "RELAXED", "HAPPY", "SUSPICIOUS", "DISAPPOINTED", "HUNGRY", "SASSY", "INSPIRED"];
-    
+
     emotionsArray.forEach(makeButton);
 
     function makeButton(element, i, array) {
@@ -10,56 +10,44 @@ $(document).ready(function () {
         emotionBtn.attr("data-name", emotion);
         emotionBtn.text(emotion);
         $("#btnsHere").append(emotionBtn);
-        console.log(emotion);
     }
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search";
+    $(".emotion").on("click", function () {
+        var searchTerm = $(this).attr("data-name");
+        console.log(searchTerm);
+        var queryURL = "https://api.giphy.com/v1/gifs/search";
 
-    $.ajax({
+        $.ajax({
         url: queryURL,
         method: "GET",
         data: {
-            q: emotion,
+            q: searchTerm,
             api_key: "EiUEVleCpKoSRlSJK8Z7BMD3sxjCIhKQ",
             limit: 10
-        }
-    }).done(function(response){
+                }
+    }).done(function (response) {
         var results = response.data;
+        console.log(results);
 
-        var pRating = $("<p>").text("Rating: " + results.rating);
-        var emotionImage = $("<img>");
-        emotionImage.attr("src", results.images.fixed_height.url);
-        $("<p>").append(emotionImage);
-        $("#emotionDiv").append(emotionImage);
+        // var pRating = $("<p>").text("Rating: " + results.rating);
+        // var emotionImage = $("<img>");
+        // emotionImage.attr("src", results.images.fixed_height.url);
+        // $("<p>").append(emotionImage);
+        // $("#emotionDiv").append(emotionImage);
     });
 
-    // //add event listener to give button a function
-    // $("#gif-button").on("click", function() {
+                //             //use the API documentation to get the image of the response
+                //             var imageUrl = response.data.image_original_url;
 
-    //           //saving API endpoint as a variable
-    //           var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=EiUEVleCpKoSRlSJK8Z7BMD3sxjCIhKQ&tag=frustrated";
+                //             //create image element where image of response will be displayed
+                //             var frustratedGif = $("<img>");
 
-    //           //sending request to API endpoint to get info
-    //           $.ajax({
-    //             url: queryURL,
-    //             method: "GET"
-    //           })
+                //             //give image attributes to display image or alt text
+                //             frustratedGif.attr("src", imageUrl);
+                //             frustratedGif.attr("alt", "frustrated gif");
 
-    //           //function to use request response
-    //           .done(function(response) {
-
-    //             //use the API documentation to get the image of the response
-    //             var imageUrl = response.data.image_original_url;
-
-    //             //create image element where image of response will be displayed
-    //             var frustratedGif = $("<img>");
-
-    //             //give image attributes to display image or alt text
-    //             frustratedGif.attr("src", imageUrl);
-    //             frustratedGif.attr("alt", "frustrated gif");
-
-    //             //add the new element before the images div
-    //             $("#images").prepend(frustratedGif);
-    //           });
-    //         });
+                //             //add the new element before the images div
+                //             $("#images").prepend(frustratedGif);
+                //           });
+    });
 });
