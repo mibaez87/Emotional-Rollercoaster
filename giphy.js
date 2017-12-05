8e7dcf32a10672076e83a869173a830fa24c807d
Michel B <michelb@JB-MBA.local>
2017-12-05 17:12:01 -0500

    working buttons and giphy static and user

--- giphy.js
+++ giphy.js
@@ -2,66 +2,28 @@ $(document).ready(function () {
     var emotionsArray = ["UNIMPRESSED", "RELAXED", "HAPPY", "SUSPICIOUS", "DISAPPOINTED", "HUNGRY", "SASSY", "INSPIRED"];
 
     emotionsArray.forEach(makeButton);
+    getGiphy();
 
     function makeButton(element, i, array) {
-        var emotion = emotionsArray[i];
-        var emotionBtn = $("<button></button><br>");
+        var emotion = element;
+        var emotionBtn = $("<button>");
         emotionBtn.addClass("emotion");
         emotionBtn.attr("data-name", emotion);
         emotionBtn.text(emotion);
-        $("#btnsHere").append(emotionBtn);
-        
-        $(document).on("click", ".emotion", function () {
-            var searchTerm = $(this).attr("data-name");
-            var queryURL = "https://api.giphy.com/v1/gifs/search";
-
-            $.ajax({
-                url: queryURL,
-                method: "GET",
-                data: {
-                    q: searchTerm,
-                    api_key: "EiUEVleCpKoSRlSJK8Z7BMD3sxjCIhKQ",
-                    limit: 10
-                }
-            }).done(function (response) {
-                var results = response.data;
-
-                for (var i = 0, len = results.length; i < len; i++) {
-                    var gifDiv = $("<div>").addClass("gif");
-
-                    var rating = results[i].rating;
-                    var pRating = $("<p>").text("Rating: " + rating);
-
-                    var emotionGif = $("<img>");
-                    emotionGif.attr("src", results[i].images.fixed_height.url);
-
-                    gifDiv.append(pRating);
-                    gifDiv.append(emotionGif);
-
-                    $("#emotionDiv").append(gifDiv);
-                }
-            });
-        });
+        $("#btnsHere").append(emotionBtn).append("<br>");
     }
 
-    $("#submitBtn").on("click", function (event) {
-        event.preventDefault();
-
-        var userChoice = $("#searchField").val().trim();
-
-        var emotionBtn = $("<button></button><br>");
-        emotionBtn.addClass("emotion");
-        emotionBtn.attr("data-name", userChoice);
-        emotionBtn.text(userChoice);
-        $("#btnsHere").append(emotionBtn);
-
+    function getGiphy() {
+    $(document).on("click", ".emotion", function () {
+        $("#emotionDiv").empty();
+        var searchTerm = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search";
 
         $.ajax({
             url: queryURL,
             method: "GET",
             data: {
-                q: userChoice,
+                q: searchTerm,
                 api_key: "EiUEVleCpKoSRlSJK8Z7BMD3sxjCIhKQ",
                 limit: 10
             }
@@ -83,5 +45,15 @@ $(document).ready(function () {
                 $("#emotionDiv").append(gifDiv);
             }
         });
+    });
+    }
+
+    $("#submitBtn").on("click", function (event) {
+        event.preventDefault();
+
+        var userChoice = $("#searchField").val().trim();
+
+        makeButton(userChoice);
+        getGiphy(userChoice);
     });
 });
